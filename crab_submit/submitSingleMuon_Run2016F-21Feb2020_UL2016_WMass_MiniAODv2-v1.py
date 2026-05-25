8fdc4c76cdac7a26bf408b0c670e96aa9637507d
Zobeyer Ghafoor <zobeyer.ghafoor@physik.uzh.ch>
2026-05-25 15:22:11 +0200

    Resubmission of failed jobs from  Data_SingleMuon and Data_MinBias to crab

--- crab_submit/submitSingleMuon_Run2016F-21Feb2020_UL2016_WMass_MiniAODv2-v1.py
+++ crab_submit/submitSingleMuon_Run2016F-21Feb2020_UL2016_WMass_MiniAODv2-v1.py
@@ -1,13 +1,13 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'SingleMuon_Run2016F-21Feb2020_UL2016_WMass_MiniAODv2-v1_24042026'
+config.General.requestName = 'SingleMuon_Run2016F-21Feb2020_UL2016_WMass_MiniAODv2-v1_24042026_Resubmission'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.numCores = 1
-config.JobType.maxMemoryMB = 2000
+config.JobType.maxMemoryMB = 3000
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/home/z/zoghafoo/CMSSW_10_6_26/src/Configuration/WMassNanoProduction/configs/NanoV9DataPostVFP_cfgCRAB.py'
 config.JobType.allowUndistributedCMSSW = True
@@ -18,8 +18,9 @@ config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
 config.Data.outLFNDirBase = '/store/user/zoghafoo/crabsubmission_files'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'NanoV9Run2016FDataPostVFP_24042026'
+config.Data.outputDatasetTag = 'NanoV9Run2016FDataPostVFP_24042026_Resubmission'
 config.Data.inputDBS = 'global'
 config.Data.useParent = False
+config.Data.lumiMask = '/home/z/zoghafoo/CMSSW_10_6_26/src/Configuration/WMassNanoProduction/crab_projects/crab_SingleMuon_Run2016F-21Feb2020_UL2016_WMass_MiniAODv2-v1_24042026/results/notFinishedLumis.json'
 
 config.Site.storageSite = 'T3_CH_PSI'
--- crab_submit/submitZeroBias_Run2016F-UL2016_MiniAODv2-v1.py
+++ crab_submit/submitZeroBias_Run2016F-UL2016_MiniAODv2-v1.py
@@ -1,13 +1,13 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'ZeroBias_Run2016F-UL2016_MiniAODv2-v1_02052026'
+config.General.requestName = 'ZeroBias_Run2016F-UL2016_MiniAODv2-v1_02052026_Resubmission'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.numCores = 1
-config.JobType.maxMemoryMB = 2000
+config.JobType.maxMemoryMB = 3000
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/home/z/zoghafoo/CMSSW_10_6_26/src/Configuration/WMassNanoProduction/configs/NanoV9DataPostVFP_MINBIAS_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
@@ -18,8 +18,9 @@ config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
 config.Data.outLFNDirBase = '/store/user/zoghafoo/crabsubmission_files'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'NanoV9Run2016FDataPostVFP_MinBias_02052026'
+config.Data.outputDatasetTag = 'NanoV9Run2016FDataPostVFP_MinBias_02052026_Resubmission'
 config.Data.inputDBS = 'global'
 config.Data.useParent = False
+config.Data.lumiMask = '/home/z/zoghafoo/CMSSW_10_6_26/src/Configuration/WMassNanoProduction/crab_projects/crab_ZeroBias_Run2016F-UL2016_MiniAODv2-v1_02052026/results/notFinishedLumis.json'
 
 config.Site.storageSite = 'T3_CH_PSI'
